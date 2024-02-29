331bc75bdc6f325d7c7d3b867575e699003809a4
rguido <rodrigoguidoarias92@gmail.com>
2024-02-29 17:04:34 +0100

    .1

--- recursos/script.sql
+++ recursos/script.sql
@@ -1,55 +1,74 @@
--- Crear la base de datos-- Crear la base de datos
-CREATE DATABASE futbol;
--- Crear el esquema "objetos"
 CREATE SCHEMA objetos;
 
+--  tipo para la informacion de la persona
 -- Cambiar al esquema "objetos"
 SET search_path TO objetos;
-
--- Crear tabla para los datos personales de los jugadores y entrenadores
-CREATE TABLE Persona (
-    nombre VARCHAR(40),
-    edad INT,
-    PRIMARY KEY (nombre)
+CREATE TYPE Persona as(
+    nombre text,
+    edad int
 );
 
--- Crear tabla para la información de los jugadores
+-- tipo para la informacion del jugador
+CREATE TYPE Jugador as(
+    dorsal int,
+    posicion text,
+	altura float
+);
+--  tipo para la informacion del equipo
+CREATE TYPE Equipo as(
+    nombre text,
+    ciudad text,
+    entrenador_persona Persona 
+);
+
+--  tabla para la informacion de los equipos
+CREATE TABLE Equipos (
+	equipo_id SERIAL PRIMARY KEY,
+	equipo_info Equipo 
+);
+
+-- tabla para la informacion de los jugadores
 CREATE TABLE Jugadores (
     jugador_id SERIAL PRIMARY KEY,
-    dorsal INT,
-    posicion VARCHAR(40),
-    altura DECIMAL,
-    nombre_persona VARCHAR(40) REFERENCES Persona(nombre)
+   datos_personales Persona,
+	jugador_info Jugador,
+    equipo_id integer not null,
+	FOREIGN KEY (equipo_id) references objetos.Equipos(equipo_id)
+	
 );
-
--- Crear tabla para la información de los equipos
-CREATE TABLE Equipos (
-    equipo_id SERIAL PRIMARY KEY,
-    nombre VARCHAR(40),
-    ciudad VARCHAR(40),
-    entrenador_persona VARCHAR(40) REFERENCES Persona(nombre)
-);
-
--- Crear tabla para los partidos
+--  tabla para los partidos
 CREATE TABLE Partidos (
     partido_id SERIAL PRIMARY KEY,
     fecha DATE,
-    equipo_local_id INT REFERENCES Equipos(equipo_id),
-    equipo_visitante_id INT REFERENCES Equipos(equipo_id)
+	equipo_local_id int not null,
+	equipo_visitante_id int not null,
+    FOREIGN KEY(equipo_local_id) REFERENCES obejtos.Equipos(equipo_id),
+   FOREIGN KEY(equipo_visitante_id)REFERENCES obejtos.Equipos(equipo_id) 
 );
 
-INSERT INTO Persona (nombre, edad) VALUES
-    ('Juan', 30),
-    ('Ruben', 25),
-    ('Pedro', 28);
-     ('Illias', 18);
-      ('Cholo', 48);
 
+INSERT INTO objetos.Jugadores (datos_personales,jugador_info,equipo_id) VALUES(
+    ROW('Juan',30),ROW(10, 'Delantero', 1.75),1
+    );
 
-INSERT INTO Jugadores (dorsal, posicion, altura, nombre_persona) VALUES
-    (10, 'Delantero', 1.75, 'Juan'),
-    (5, 'Defensa', 1.80, 'Pedro');
+INSERT INTO objetos.Equipos (equipo_info) VALUES(
+    Row('Vigo', 'celta', ROW('Cholo',22))
+	
+);
+INSERT INTO objetos.Equipos (equipo_info) VALUES(
+    Row('Madrid', 'Real', ROW('Ance',66))
+	
+);
+INSERT INTO objetos.Equipos (equipo_info) VALUES(
+    Row('Depor', 'Coruña', ROW('Cholo',22))
+	
+);
+INSERT INTO objetos.Equipos (equipo_info) VALUES(
+    Row('Vigo', 'celta', ROW('Cholo',22))
+	
+);
 
-INSERT INTO Equipos (nombre, ciudad, entrenador_persona) VALUES
-    ('Cultural', 'Leon', 'Ruben'),
-    ('Pontevedra', 'Pontevedra', 'Cholo');
+INSERT INTO objetos.Partidos (date,equipo_local_id,equipo_visitante_id) VALUES(
+    '13-03-2022',1,2
+	
+);
--- src/App.java
+++ src/App.java
@@ -1,5 +1,176 @@
+import java.sql.*;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
+    /**
+     * Tareas a realizar
+     * Se pide:
+     *
+     * Crear el script .sql que permita crear la base de datos PostgreSQL futbol.
+     *
+     * El script debe permitir crear la estructura de la base de datos.
+     * Debe insertar datos suficientes que permita verificar el correcto funcionamiento de las siguientes consultas.
+     * Crear los métodos que permitan:
+     *
+     * insertar, eliminar y modificar un Equipo.
+     * insertar, eliminar y modificar un Jugador.
+     * insertar, eliminar y modificar un Partido.
+     * Inscribir y desinscribir un Jugador de un Equipo.
+     * Crear métodos para realizar las siguientes consultas:
+     *
+     * Listar toda la información de un Equipo buscándolo por id.
+     * Listar toda la información de todos los Equipos.
+     * Listar la información de un Jugador buscándolo por id.
+     * Listar la información de un Jugador buscándolo por nombre.
+     * Buscar partidos en los que un determinado equipo jugara como local.
+     * Buscar partidos en los que un determinado equipo jugara como visitante.
+     * Obtener toda la información de los jugadores que jueguen en una determinada posición.
+     * Obtener toda la información de los jugadores según su dorsal.
+     * Obtener todos los partidos según la fecha.
+     */
+    private static Connection connection;
+    static Scanner sc;
+    private static PreparedStatement pStatement = null;
+    private static Statement statement = null;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String url = "jdbc:postgresql://localhost/futbol";
+        String user = "postgres";
+        String password = "abc123";
+        int option = -1;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            sc = new Scanner(System.in);
+            do {
+                System.out.println("1.- Insertar equipo\n2.-Insertar jugador\n3.- Insertar partido");
+                option = sc.nextInt();
+                switch (option) {
+                    case 1:
+                        insertarEquipo(pedirString("nombre equipo"), pedirString("ciudad equipo"),
+                                pedirString("nombre entrenador"), pedirInt("edad entrenador"));
+                        break;
+                    case 2:
+                        insertarJugador(pedirString("Nombre jugador"),pedirInt("Edad jugador"),pedirInt("Dorsal")
+                                ,pedirString("Posicion"),pedirFloat("Altura"),pedirInt("Id del equipo del jugador"));
+                        break;
+                    case 3:
+                        insertarPartido(pedirFecha("Introduce una fecha"),pedirInt("Introduce id equipo local"),pedirInt("Introduce id equipo visitante"));
+                        break;
+                    default:
+                        break;
+                }
+            } while (option != 0);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+    }
+
+    private static void insertarPartido(Date fecha, int id_equipolocal, int id_equipovisitante) {
+        String query = "INSERT INTO objetos.Partidos (fecha,equipo_local_id,equipo_visitante_id) VALUES(?,?,?)" ;
+        try {
+            pStatement = connection.prepareStatement(query);
+            pStatement.setDate(1, fecha);
+            pStatement.setInt(2, id_equipolocal);
+            pStatement.setInt(3, id_equipovisitante);
+            int filasAfectadas = pStatement.executeUpdate();
+            System.out.println("filas afectadas  = " + filasAfectadas);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static Date pedirFecha(String mensaje) {
+        LocalDate fecha = LocalDate.of(pedirInt("Introduce el año"),
+                pedirInt("Introduce numero de mes"), pedirInt("Introduce dia del mes"));
+        Date date = Date.valueOf(fecha);
+        return  date;
+    }
+
+
+
+    private static Float pedirFloat(String mensaje) {
+        float entrada=0f;
+        System.out.println(mensaje);
+        try {
+            entrada = sc.nextFloat();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Debes introducir un float");
+        }
+        sc.nextLine();
+        return  entrada;
+    }
+
+    public static String pedirString(String mensaje) {
+        System.out.println(mensaje);
+        String entrada = "";
+            try {
+                entrada = sc.next();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Debes introducir un string");
+
+            }
+        sc.nextLine();
+
+        return entrada;
+    }
+    public static int pedirInt(String mensaje) {
+        System.out.println(mensaje);
+        int entrada = -1;
+
+            try {
+                entrada = sc.nextInt();
+            } catch (InputMismatchException e) {
+              //  e.printStackTrace();
+                System.out.println("Debes introducir un integer");
+                sc.nextLine();
+            }
+
+
+        return entrada;
+    }
+    public static void insertarEquipo(String nombre, String ciudad, String nombre_entrenador, int edad) {
+
+        String query = "INSERT INTO objetos.Equipos(equipo_info) VALUES(Row(?, ?, ROW(?,?)))";
+        try {
+            pStatement = connection.prepareStatement(query);
+            pStatement.setString(1, nombre);
+            pStatement.setString(2, ciudad);
+            pStatement.setString(3, nombre_entrenador);
+            pStatement.setInt(4, edad);
+
+            int filasAfectadas = pStatement.executeUpdate();
+            System.out.println("filas afectadas  = " + filasAfectadas);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void insertarJugador(String nombre, int edad, int dorsal,String posicion,float altura,int equipo_id) {
+
+        String query = "INSERT INTO objetos.Jugadores (datos_personales,jugador_info,equipo_id) VALUES(" +
+                " ROW(?,?),ROW(?, ?, ?),?)" ;
+        try {
+            pStatement = connection.prepareStatement(query);
+            pStatement.setString(1, nombre);
+            pStatement.setInt(2, edad);
+            pStatement.setInt(3, dorsal);
+            pStatement.setString(4, posicion);
+            pStatement.setFloat(5, altura);
+            pStatement.setInt(6, equipo_id);
+
+            int filasAfectadas = pStatement.executeUpdate();
+            System.out.println("filas afectadas  = " + filasAfectadas);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
